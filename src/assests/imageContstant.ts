aa726641eddc9c6fe114f306ac03ad6b0b31dd93
Mrdynamic-soni <ssoni5930@gmail.com>
2024-05-05 23:26:21 +0530

    fixed import

--- src/assests/imageContstant.ts
+++ src/assests/imageContstant.ts
@@ -1,20 +1,9 @@
-// export const DASHBOARD: string = require('./images/dashbord.png')
-// export const DOCUMENT: string = require('./images/documents.png')
-// export const AURORA: string = require('./images/aurora.png')
-// export const ACCOUNTING: string = require('./images/accounting.png')
-// export const BANKING: string = require('./images/banking.png')
-// export const BG_IMG: string = require('./images/image.png')
-// export const ORG_IMG: string = require('./images/org.png')
-// export const PURCHASE: string = require('./images/purchase.png')
-// export const REPORT: string = require('./images/report.png')
-// export const SALES: string = require('./images/sales.png')
-
 import DASHBOARD from './images/dashboard.png';
 import DOCUMENT from './images/documents.png';
 import AURORA from './images/aurora.png';
 import ACCOUNTING from './images/accounting.png';
 import BANKING from './images/banking.png';
-import BG_IMG from './images/image.png';
+import BG_IMG from './images/Image.png';
 import ORG_IMG from './images/org.png';
 import PURCHASE from './images/purchase.png';
 import REPORT from './images/report.png';
